cc5225a4b9a7f46f7efbe9395fd477784b6040af
lstmemery <matthew.emery44@gmail.com>
2021-04-11 16:24:38 -0700

    Initial commit

---
+++ strang4.4.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.14.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f371bdae-9b1c-11eb-0d7e-6b6f1b89050b
+using LinearAlgebra
+
+# ╔═╡ Cell order:
+# ╠═f371bdae-9b1c-11eb-0d7e-6b6f1b89050b
